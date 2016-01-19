1a4ed725e46105c15621cd8593b68ce6d7cb6f10
AlexKolokolov <Kolokolov_a@ukr.net>
2016-01-19 19:18:39 +0200

    some refactoring was performed

--- src/main/java/ua/djhans/Messenger.java
+++ src/main/java/ua/djhans/Messenger.java
@@ -3,17 +3,17 @@ package ua.djhans;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
- * Класс содержит статический метод выдающий сообщение, которое зависит от переданных методу
- * значения часа (от 0 до 23) и локали пользователя.
+ * Класс для генерации сообщений-приветствий.
+ * Содержит статический метод выдающий сообщение, которое зависит от переданных методу
+ * значения часа (в формате от 0 до 23) и локали. Если язык системы - русский, то сообщения
+ * выводятся на русском, иначе - на английском.
  */
-
 public class Messenger {
     private static Logger logger = LogManager.getLogger(Messenger.class);
 
@@ -23,14 +23,11 @@ public class Messenger {
         logger.debug("Got current locale: " + locale);
 
         locale = locale == null || !"ru".equals(locale.getLanguage()) ? Locale.ENGLISH : locale;
-        currentHour = currentHour > 23 || currentHour < 0 ? 0 : currentHour;
 
-        logger.debug("Current hour after validation: " + currentHour);
         logger.debug("Locale after validation: " + locale);
 
         ResourceBundle bundle = ResourceBundle.getBundle("Messages", locale);
         String timeOfDay = "";
-
         if (currentHour >= 6 && currentHour < 9) timeOfDay = "morning";
         if (currentHour >= 9 && currentHour < 19) timeOfDay = "day";
         if (currentHour >= 19 && currentHour < 23) timeOfDay = "evening";
@@ -46,10 +43,13 @@ public class Messenger {
         return message;
     }
 
-    public static void main(String[] args) {
+    public static void printCurrentTimeMessage() {
         int currentHour = new GregorianCalendar().get(Calendar.HOUR_OF_DAY);
         Locale locale = Locale.getDefault();
-
         System.out.println(getMessage(currentHour, locale));
+    }
+
+    public static void main(String[] args) {
+        printCurrentTimeMessage();
     }
 }
