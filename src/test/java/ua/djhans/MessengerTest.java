7e2367dc81b9c99f5054827ee2fa84d74f2df343
AlexKolokolov <Kolokolov_a@ukr.net>
2016-01-21 19:47:35 +0200

    Jacoco test coverage analyzer was added. Code duplication in MessengerTest was eliminated. Comments to pom.xml were added.

--- src/test/java/ua/djhans/MessengerTest.java
+++ src/test/java/ua/djhans/MessengerTest.java
@@ -10,88 +10,174 @@ import java.util.Locale;
  */
 public class MessengerTest {
 
-    @Test
-    public void getMessageTestRu4() {
-        Locale locale = new Locale("ru");
-        int hourOfDay = 4;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Спокойной ночи, Мир!", message);
+    private void getMessageTestRu(int hourOfDay) {
+        String message = Messenger.getMessage(hourOfDay, new Locale("ru"));
+        if (hourOfDay >= 6 && hourOfDay < 9) Assert.assertEquals("Доброе утро, Мир!", message);
+        if (hourOfDay >= 9 && hourOfDay < 19) Assert.assertEquals("Добрый день, Мир!", message);
+        if (hourOfDay >= 19 && hourOfDay < 23) Assert.assertEquals("Добрый вечер, Мир!", message);
+        if (hourOfDay >= 23 || hourOfDay < 6) Assert.assertEquals("Спокойной ночи, Мир!", message);
+    }
+
+    private void getMessageTestNotRu(int hourOfDay, Locale notRussianLocale) {
+        String message = Messenger.getMessage(hourOfDay, notRussianLocale);
+        if (hourOfDay >= 6 && hourOfDay < 9) Assert.assertEquals("Good morning, World!", message);
+        if (hourOfDay >= 9 && hourOfDay < 19) Assert.assertEquals("Good day, World!", message);
+        if (hourOfDay >= 19 && hourOfDay < 23) Assert.assertEquals("Good evening, World!", message);
+        if (hourOfDay >= 23 || hourOfDay < 6) Assert.assertEquals("Good night, World!", message);
     }
 
     @Test
-    public void getMessageTestRu6() {
-        Locale locale = new Locale("ru");
-        int hourOfDay = 6;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Доброе утро, Мир!", message);
+    public void testRu0h() {
+        getMessageTestRu(0);
     }
 
     @Test
-    public void getMessageTestRu12() {
-        Locale locale = new Locale("ru");
-        int hourOfDay = 12;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Добрый день, Мир!", message);
+    public void testRu2h() {
+        getMessageTestRu(2);
     }
 
     @Test
-    public void getMessageTestEn8() {
-        Locale locale = Locale.ENGLISH;
-        int hourOfDay = 8;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Good morning, World!", message);
+    public void testRu6h() {
+        getMessageTestRu(6);
     }
 
     @Test
-    public void getMessageTestEn22() {
-        Locale locale = Locale.ENGLISH;
-        int hourOfDay = 22;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Good evening, World!", message);
+    public void testRu8h() {
+        getMessageTestRu(8);
     }
 
     @Test
-    public void getMessageTestDe7() {
-        Locale locale = Locale.GERMAN;
-        int hourOfDay = 7;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Good morning, World!", message);
+    public void testRu9h() {
+        getMessageTestRu(9);
     }
 
     @Test
-    public void getMessageTestDe19() {
-        Locale locale = Locale.GERMAN;
-        int hourOfDay = 19;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Good evening, World!", message);
+    public void testRu12h() {
+        getMessageTestRu(12);
     }
 
     @Test
-    public void getMessageTestUnknown8() {
-        int hourOfDay = 8;
-        String message = Messenger.getMessage(hourOfDay, null);
-        Assert.assertEquals("Good morning, World!", message);
+    public void testRu18h() {
+        getMessageTestRu(18);
     }
 
     @Test
-    public void getMessageTestUnknown20() {
-        int hourOfDay = 20;
-        String message = Messenger.getMessage(hourOfDay, null);
-        Assert.assertEquals("Good evening, World!", message);
+    public void testRu19h() {
+        getMessageTestRu(19);
     }
 
     @Test
-    public void getMessageTestRuWrongTime() {
-        Locale locale = new Locale("ru");
-        int hourOfDay = -5;
-        String message = Messenger.getMessage(hourOfDay, locale);
-        Assert.assertEquals("Спокойной ночи, Мир!", message);
+    public void testRu21h() {
+        getMessageTestRu(21);
     }
 
     @Test
-    public void getMessageTestUnknownWrongTime() {
-        int hourOfDay = 50;
-        String message = Messenger.getMessage(hourOfDay, null);
-        Assert.assertEquals("Good night, World!", message);
+    public void testRu23h() {
+        getMessageTestRu(0);
+    }
+
+    @Test
+    public void testRuWrongTime1() {
+        getMessageTestRu(50);
+    }
+
+    @Test
+    public void testRuWrongTime2() {
+        getMessageTestRu(-50);
+    }
+
+    @Test
+    public void testEn0h() {
+        getMessageTestNotRu(0, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn4h() {
+        getMessageTestNotRu(4, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn6h() {
+        getMessageTestNotRu(6, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn9h() {
+        getMessageTestNotRu(9, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn13h() {
+        getMessageTestNotRu(13, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn17h() {
+        getMessageTestNotRu(17, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn19h() {
+        getMessageTestNotRu(19, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEn23h() {
+        getMessageTestNotRu(23, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEnWrongTime1() {
+        getMessageTestNotRu(100, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testEnWrongTime2() {
+        getMessageTestNotRu(-100, Locale.ENGLISH);
+    }
+
+    @Test
+    public void testGe0h() {
+        getMessageTestNotRu(0, Locale.GERMAN);
+    }
+
+    @Test
+    public void testGe6h() {
+        getMessageTestNotRu(0, Locale.GERMAN);
+    }
+
+    @Test
+    public void testGe19h() {
+        getMessageTestNotRu(19, Locale.GERMAN);
+    }
+
+    @Test
+    public void testGeWrongTime() {
+        getMessageTestNotRu(1000, Locale.GERMAN);
+    }
+
+    @Test
+    public void testFr8h() {
+        getMessageTestNotRu(8, Locale.FRENCH);
+    }
+
+    @Test
+    public void testFr19h() {
+        getMessageTestNotRu(19, Locale.FRENCH);
+    }
+
+    @Test
+    public void testUnknownLang9h() {
+        getMessageTestNotRu(9, null);
+    }
+
+    @Test
+    public void testUnknownLang19h() {
+        getMessageTestNotRu(19, null);
+    }
+
+    @Test
+    public void testUnknownLangWrongTime() {
+        getMessageTestNotRu(-1000, null);
     }
 }
